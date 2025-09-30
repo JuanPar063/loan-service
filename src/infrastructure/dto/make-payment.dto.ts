79a61404d03787a15caf9c5c4054ce6acdef38fd
fapallares19 <142366964+fapallares19@users.noreply.github.com>
2025-09-29 21:27:39 -0500

    Update make-payment.dto.ts

--- src/infrastructure/dto/make-payment.dto.ts
+++ src/infrastructure/dto/make-payment.dto.ts
@@ -1,6 +1,11 @@
 import { IsNumber, IsPositive } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class MakePaymentDto {
+  @ApiProperty({
+    description: 'Monto del pago a realizar',
+    example: 200,
+  })
   @IsNumber()
   @IsPositive()
   amount: number;
